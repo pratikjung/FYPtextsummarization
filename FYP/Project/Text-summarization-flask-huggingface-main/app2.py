de031feb1999b5d0afd1daa51974d18caf13fc18
pratikjung <84096115+pratikjung@users.noreply.github.com>
2023-05-04 13:00:28 +0545

    Add files via upload

---
+++ FYP/Project/Text-summarization-flask-huggingface-main/app2.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import torch
+
+app = Flask(__name__)
+
+model_path = "Users\Ripple\Desktop\FYP\Text-summarization-flask-huggingface-main"
+model_name_or_path = "transformersbook/pegasus-samsum"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+tokenizer = PegasusTokenizer.from_pretrained(model_name_or_path, cache_dir=model_path)
+model = PegasusForConditionalGeneration.from_pretrained(model_name_or_path, cache_dir=model_path).to(device)
+
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/text-summarization', methods=["POST"])
+def summarize():
+
+    if request.method == "POST":
+
+        inputtext = request.form["inputtext_"]
+
+        input_text = "summarize: " + inputtext
+
+        tokenized_text = tokenizer.encode(input_text, return_tensors='pt', max_length=512).to(device)
+        summary_ = model.generate(tokenized_text, min_length=90, max_length=300)
+        summary = tokenizer.decode(summary_[0], skip_special_tokens=True)
+
+
+
+    return render_template("output.html", data = {"summary": summary})
+
+if __name__ == '__main__': # It Allows You to Execute Code When the File Runs as a Script
+    app.run(port=5002)
+
